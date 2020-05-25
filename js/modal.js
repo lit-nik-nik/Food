21b25da9b89a0a777cc1830780f2621beae90894
Nikolay <11arhangel05@mail.ru>
2020-05-25 13:14:08 +0300

    add open and close modal

--- js/modal.js
+++ js/modal.js
@@ -2,6 +2,28 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
+    const btn = document.querySelectorAll('button[data-modal]'),
+          modal = document.querySelector('.modal'),
+          close = document.querySelector('div[data-close]');
     
-    
+    function displayModal (elem) {
+        const style = window.getComputedStyle(elem);
+
+        if (style.display == 'none') {
+            elem.style.display = 'block';
+        } else {
+            elem.style.display = 'none';
+        }
+    }
+
+    close.addEventListener('click', () => {
+        displayModal(modal);
+    });
+
+    btn.forEach (item => {
+        item.addEventListener('click', () => {
+            displayModal(modal);
+        });
+    });
+
 });
